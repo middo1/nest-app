a9475d202e811397cbf2df9abffd4acc5b09b1b0
middo1 <hamidadeshina1@gmail.com>
2022-04-05 06:44:51 +0100

    okay now nko

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StudentController } from '../student/student.controller';
-//import { TeacherController } from '../teacher/teacher.controller';
+import { TeacherController } from '../teacher/teacher.controller';
 
 
 @Module({
   imports: [],
-  controllers: [StudentController]
+  controllers: [StudentController, TeacherController]
 })
 export class AppModule {}
